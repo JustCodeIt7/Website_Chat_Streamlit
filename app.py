2d2d737a1cb7b65e288210c7ea2fac599f9cf567
James <jamesbrendamour7@yahoo.com>
2025-03-18 04:31:25 -0600

    Add basic Streamlit app for chat functionality

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title("Website Chat Streamlit")
+
+if st.button("Start Chat"):
+    st.write("Chat started!")
+    
+if st.button("End Chat"):
+    st.write("Chat ended!")
+    
+    
+
+    
+
